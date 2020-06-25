4f29b4bbb7ce54eb4adcc0d35dd55992234404e9
Diego Sebastian Herrera <diherrer@everis.com>
2020-06-25 15:00:26 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { Actor } from './Entidades/Actor';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'router00';
+export class AppComponent implements OnInit {
+  title = 'Form';
 
-  
+  constructor() { }
 
-  constructor() {
-    
+  ngOnInit() {
+
+    const sqnc = new Observable(countOnetoTen);
+
+
+
+    sqnc.subscribe(
+      val => { console.log(val) },
+      error => { console.log("error") },
+      () => { console.log("Completed") }
+    );
+
+    function countOnetoTen(observer) {
+
+      setTimeout(() => {   
+        observer.next(1000);
+      }, 300);
+
+      for (var i = 1; i <= 10; i++) {
+        // notification 
+        observer.next(i);
+      }
+      observer.complete();
+      return { unsubscribe() { } };
+    }
   }
+
+
+
 }
